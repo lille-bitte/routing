40ac8c38a13b9794b630241d7776f55d03c8c09c
Paulus Gandung <gandung@slackerc0de.us>
2020-02-23 18:13:59 +0700

    Adding src/Response/ResponseAccessorInterface.php

---
+++ src/Response/ResponseAccessorInterface.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LilleBitte\Routing\Response;
+
+use Psr\Http\Message\ResponseInterface;
+
+/**
+ * @author Paulus Gandung Prakosa <rvn.plvhx@gmail.com>
+ */
+interface ResponseAccessorInterface
+{
+	/**
+	 * Get dispatched response status.
+	 *
+	 * @return integer
+	 */
+	public function getStatus();
+
+	/**
+	 * Get dispatched response object.
+	 *
+	 * @return ResponseInterface
+	 */
+	public function getResponse();
+}
