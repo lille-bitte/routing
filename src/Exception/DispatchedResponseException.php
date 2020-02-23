58a41cae832b268de85731445fcad73df0e4a958
Paulus Gandung <gandung@slackerc0de.us>
2020-02-23 18:10:25 +0700

    Adding src/Exception/DispatchedResponseException.php

---
+++ src/Exception/DispatchedResponseException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace LilleBitte\Routing\Exception;
+
+use Exception;
+
+/**
+ * @author Paulus Gandung Prakosa <rvn.plvhx@gmail.com>
+ */
+class DispatchedResponseException extends Exception
+{
+}
