dc85150beacbb92469571f5ce12b8b3ec498bc60
plvhx <gandung@slackerc0de.us>
2020-07-28 16:16:17 +0700

    Refactoring src/RouterInterface.php

--- src/RouterInterface.php
+++ src/RouterInterface.php
@@ -9,77 +9,77 @@ use Psr\Http\Message\RequestInterface;
  */
 interface RouterInterface
 {
-	/**
-	 * Register given route with more than one
-	 * HTTP method.
-	 *
-	 * @param array $methods List of HTTP method.
-	 * @param string $route Route path.
-	 * @param mixed $handler Handler if route has matched.
-	 * @return void
-	 */
-	public function any(array $methods, string $route, $handler, array $pattern = []);
+    /**
+     * Register given route with more than one
+     * HTTP method.
+     *
+     * @param array $methods List of HTTP method.
+     * @param string $route Route path.
+     * @param mixed $handler Handler if route has matched.
+     * @return void
+     */
+    public function any(array $methods, string $route, $handler, array $pattern = []);
 
-	/**
-	 * Register given route with GET HTTP method.
-	 *
-	 * @param string $route Route path.
-	 * @param mixed $handler Handler if route has matched.
-	 * @return void
-	 */
-	public function get(string $route, $handler, array $pattern = []);
+    /**
+     * Register given route with GET HTTP method.
+     *
+     * @param string $route Route path.
+     * @param mixed $handler Handler if route has matched.
+     * @return void
+     */
+    public function get(string $route, $handler, array $pattern = []);
 
-	/**
-	 * Register given route with POST HTTP method.
-	 *
-	 * @param string $route Route path.
-	 * @param mixed $handler Handler if route has matched.
-	 * @return void
-	 */
-	public function post(string $route, $handler, array $pattern = []);
+    /**
+     * Register given route with POST HTTP method.
+     *
+     * @param string $route Route path.
+     * @param mixed $handler Handler if route has matched.
+     * @return void
+     */
+    public function post(string $route, $handler, array $pattern = []);
 
-	/**
-	 * Register given route with PUT HTTP method.
-	 *
-	 * @param string $route Route path.
-	 * @param mixed $handler Handler if route has matched.
-	 * @return void
-	 */
-	public function put(string $route, $handler, array $pattern = []);
+    /**
+     * Register given route with PUT HTTP method.
+     *
+     * @param string $route Route path.
+     * @param mixed $handler Handler if route has matched.
+     * @return void
+     */
+    public function put(string $route, $handler, array $pattern = []);
 
-	/**
-	 * Register given route with PATCH HTTP method.
-	 *
-	 * @param string $route Route path.
-	 * @param mixed $handler Handler if route has matched.
-	 * @return void
-	 */
-	public function patch(string $route, $handler, array $pattern = []);
+    /**
+     * Register given route with PATCH HTTP method.
+     *
+     * @param string $route Route path.
+     * @param mixed $handler Handler if route has matched.
+     * @return void
+     */
+    public function patch(string $route, $handler, array $pattern = []);
 
-	/**
-	 * Register given route with DELETE HTTP method.
-	 *
-	 * @param string $route Route path.
-	 * @param mixed $handler Handler if route has matched.
-	 * @return void
-	 */
-	public function delete(string $route, $handler, array $pattern = []);
+    /**
+     * Register given route with DELETE HTTP method.
+     *
+     * @param string $route Route path.
+     * @param mixed $handler Handler if route has matched.
+     * @return void
+     */
+    public function delete(string $route, $handler, array $pattern = []);
 
-	/**
-	 * Group list of routes by route prefix.
-	 *
-	 * @param string $group Group prefix.
-	 * @param callable $callback Callback.
-	 * @return void
-	 */
-	public function group(string $group, callable $callback);
+    /**
+     * Group list of routes by route prefix.
+     *
+     * @param string $group Group prefix.
+     * @param callable $callback Callback.
+     * @return void
+     */
+    public function group(string $group, callable $callback);
 
-	/**
-	 * Dispatch matched route path with given
-	 * HTTP method.
-	 *
-	 * @param RequestInterface $request Request object.
-	 * @return array
-	 */
-	public function dispatch(RequestInterface $request);
+    /**
+     * Dispatch matched route path with given
+     * HTTP method.
+     *
+     * @param RequestInterface $request Request object.
+     * @return array
+     */
+    public function dispatch(RequestInterface $request);
 }
