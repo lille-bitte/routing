f9a7162e5f6166457a8612a1be8a71f02ed6fae4
plvhx <gandung@slackerc0de.us>
2020-07-28 16:16:41 +0700

    Refactoring src/Annotation/Method.php

--- src/Annotation/Method.php
+++ src/Annotation/Method.php
@@ -7,45 +7,45 @@ namespace LilleBitte\Routing\Annotation;
  */
 class Method
 {
-	/**
-	 * @var array
-	 */
-	private $methods;
+    /**
+     * @var array
+     */
+    private $methods;
 
-	public function __construct($methods = [])
-	{
-		$this->setMethods($methods);
-	}
+    public function __construct($methods = [])
+    {
+        $this->setMethods($methods);
+    }
 
-	/**
-	 * Add HTTP method to method list.
-	 *
-	 * @param string $method HTTP method.
-	 * @return void
-	 */
-	public function addMethod(string $method)
-	{
-		$this->methods[] = $method;
-	}
+    /**
+     * Add HTTP method to method list.
+     *
+     * @param string $method HTTP method.
+     * @return void
+     */
+    public function addMethod(string $method)
+    {
+        $this->methods[] = $method;
+    }
 
-	/**
-	 * Set HTTP methods list.
-	 *
-	 * @param array $methods HTTP methods list.
-	 * @return void
-	 */
-	public function setMethods(array $methods)
-	{
-		$this->methods = $methods;
-	}
+    /**
+     * Set HTTP methods list.
+     *
+     * @param array $methods HTTP methods list.
+     * @return void
+     */
+    public function setMethods(array $methods)
+    {
+        $this->methods = $methods;
+    }
 
-	/**
-	 * Get HTTP methods list.
-	 *
-	 * @return array
-	 */
-	public function getMethods()
-	{
-		return $this->methods;
-	}
+    /**
+     * Get HTTP methods list.
+     *
+     * @return array
+     */
+    public function getMethods()
+    {
+        return $this->methods;
+    }
 }
