9dd350c503af5956dbeb9227cf28c4ae2a2da2d5
plvhx <gandung@slackerc0de.us>
2020-07-28 16:16:55 +0700

    Refactoring src/Annotation/Route.php

--- src/Annotation/Route.php
+++ src/Annotation/Route.php
@@ -7,67 +7,67 @@ namespace LilleBitte\Routing\Annotation;
  */
 class Route
 {
-	/**
-	 * @var string
-	 */
-	private $route;
+    /**
+     * @var string
+     */
+    private $route;
 
-	/**
-	 * @var array
-	 */
-	private $method;
+    /**
+     * @var array
+     */
+    private $method;
 
-	public function __construct($path = null, array $data = [])
-	{
-		$this->setRoute(isset($data['path']) ? $data['path'] : $path);
-		$this->setMethod(
-			isset($data['method'])
-				? ($data['method'] instanceof Method
-					? $data['method']->getMethods()
-					: null)
-				: ['GET']
-		);
-	}
+    public function __construct($path = null, array $data = [])
+    {
+        $this->setRoute(isset($data['path']) ? $data['path'] : $path);
+        $this->setMethod(
+            isset($data['method'])
+                ? ($data['method'] instanceof Method
+                    ? $data['method']->getMethods()
+                    : null)
+                : ['GET']
+        );
+    }
 
-	/**
-	 * Set URI route.
-	 *
-	 * @param string $route URI route.
-	 * @return void
-	 */
-	public function setRoute($route)
-	{
-		$this->route = $route;
-	}
+    /**
+     * Set URI route.
+     *
+     * @param string $route URI route.
+     * @return void
+     */
+    public function setRoute($route)
+    {
+        $this->route = $route;
+    }
 
-	/**
-	 * Get URI route.
-	 *
-	 * @return string
-	 */
-	public function getRoute()
-	{
-		return $this->route;
-	}
+    /**
+     * Get URI route.
+     *
+     * @return string
+     */
+    public function getRoute()
+    {
+        return $this->route;
+    }
 
-	/**
-	 * Set HTTP method.
-	 *
-	 * @param array $method HTTP method.
-	 * @return void
-	 */
-	public function setMethod(array $method)
-	{
-		$this->method = $method;
-	}
+    /**
+     * Set HTTP method.
+     *
+     * @param array $method HTTP method.
+     * @return void
+     */
+    public function setMethod(array $method)
+    {
+        $this->method = $method;
+    }
 
-	/**
-	 * Get HTTP method.
-	 *
-	 * @return string
-	 */
-	public function getMethod()
-	{
-		return $this->method;
-	}
+    /**
+     * Get HTTP method.
+     *
+     * @return string
+     */
+    public function getMethod()
+    {
+        return $this->method;
+    }
 }
