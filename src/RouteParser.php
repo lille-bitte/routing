586a543c4b5ba81e6343b472c68d8d89186778e4
plvhx <gandung@slackerc0de.us>
2020-07-28 16:15:44 +0700

    Refactoring src/RouteParser.php

--- src/RouteParser.php
+++ src/RouteParser.php
@@ -15,113 +15,113 @@ use function join;
  */
 class RouteParser
 {
-	/**
-	 * @var array
-	 */
-	private $segments = [];
+    /**
+     * @var array
+     */
+    private $segments = [];
 
-	/**
-	 * @var array
-	 */
-	private $parameters = [];
+    /**
+     * @var array
+     */
+    private $parameters = [];
 
-	/**
-	 * @var string
-	 */
-	private const REGEX_SPLITTER = '/([a-z0-9\-\_][a-z0-9\-\_]*)|([^\{\}\s\/]+)|\s*|(.)/x';
+    /**
+     * @var string
+     */
+    private const REGEX_SPLITTER = '/([a-z0-9\-\_][a-z0-9\-\_]*)|([^\{\}\s\/]+)|\s*|(.)/x';
 
-	/**
-	 * @var string
-	 */
-	private const DEFAULT_PLACEHOLDER_REGEX = '[^/]+';
+    /**
+     * @var string
+     */
+    private const DEFAULT_PLACEHOLDER_REGEX = '[^/]+';
 
-	/**
-	 * Parse given route to two tuples, static routes and
-	 * route parameters.
-	 *
-	 * @param string $route Route path.
-	 * @param array $pattern Route parameters pattern.
-	 * @return void
-	 */
-	public function parse(string $route, array $pattern = [])
-	{
-		$res = preg_split(
-			self::REGEX_SPLITTER,
-			$route,
-			-1,
-			PREG_SPLIT_NO_EMPTY | PREG_SPLIT_DELIM_CAPTURE
-		);
+    /**
+     * Parse given route to two tuples, static routes and
+     * route parameters.
+     *
+     * @param string $route Route path.
+     * @param array $pattern Route parameters pattern.
+     * @return void
+     */
+    public function parse(string $route, array $pattern = [])
+    {
+        $res = preg_split(
+            self::REGEX_SPLITTER,
+            $route,
+            -1,
+            PREG_SPLIT_NO_EMPTY | PREG_SPLIT_DELIM_CAPTURE
+        );
 
-		$pos = $index = 0;
+        $pos = $index = 0;
 
-		while (isset($res[$pos])) {
-			if ($res[$pos] === '/' || $res[$pos] === '}') {
-				$pos++;
-				continue;
-			}
+        while (isset($res[$pos])) {
+            if ($res[$pos] === '/' || $res[$pos] === '}') {
+                $pos++;
+                continue;
+            }
 
-			if ($res[$pos] === '{') {
-				if ($res[$pos + 1] === '}') {
-					throw new MatcherException(
-						"Placeholder must not be empty."
-					);
-				}
+            if ($res[$pos] === '{') {
+                if ($res[$pos + 1] === '}') {
+                    throw new MatcherException(
+                        "Placeholder must not be empty."
+                    );
+                }
 
-				$val = $res[$pos + 1];
+                $val = $res[$pos + 1];
 
-				$tmp = [
-					'index' => $index++,
-					'value' => $val,
-					'default' => self::DEFAULT_PLACEHOLDER_REGEX
-				];
+                $tmp = [
+                    'index' => $index++,
+                    'value' => $val,
+                    'default' => self::DEFAULT_PLACEHOLDER_REGEX
+                ];
 
-				if (isset($pattern[$val])) {
-					$tmp['pattern'] = $pattern[$val];
-				}
+                if (isset($pattern[$val])) {
+                    $tmp['pattern'] = $pattern[$val];
+                }
 
-				$this->parameters[] = $tmp;
+                $this->parameters[] = $tmp;
 
-				$pos += 2;
-				continue;
-			}
+                $pos += 2;
+                continue;
+            }
 
-			$this->segments[] = [
-				'index' => $index++,
-				'value' => $res[$pos]
-			];
+            $this->segments[] = [
+                'index' => $index++,
+                'value' => $res[$pos]
+            ];
 
-			$pos++;
-		}
-	}
+            $pos++;
+        }
+    }
 
-	/**
-	 * Reset parser state.
-	 *
-	 * @return void
-	 */
-	public function reset()
-	{
-		$this->segments = $this->parameters = [];
-	}
+    /**
+     * Reset parser state.
+     *
+     * @return void
+     */
+    public function reset()
+    {
+        $this->segments = $this->parameters = [];
+    }
 
-	/**
-	 * Get route static segments
-	 * metadata.
-	 *
-	 * @return array
-	 */
-	public function getSegments()
-	{
-		return $this->segments;
-	}
+    /**
+     * Get route static segments
+     * metadata.
+     *
+     * @return array
+     */
+    public function getSegments()
+    {
+        return $this->segments;
+    }
 
-	/**
-	 * Get route parameters metadata.
-	 *
-	 * @return array
-	 */
-	public function getParameters()
-	{
-		return $this->parameters;
-	}
+    /**
+     * Get route parameters metadata.
+     *
+     * @return array
+     */
+    public function getParameters()
+    {
+        return $this->parameters;
+    }
 }
