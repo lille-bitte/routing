70baa087dcbe41c7796ec92edafd8d4f20e404b4
plvhx <gandung@slackerc0de.us>
2020-07-28 16:15:30 +0700

    Refactoring src/RouteAggregator.php

--- src/RouteAggregator.php
+++ src/RouteAggregator.php
@@ -9,103 +9,103 @@ use function rtrim;
  */
 class RouteAggregator
 {
-	/**
-	 * @var array
-	 */
-	private $routes = [];
+    /**
+     * @var array
+     */
+    private $routes = [];
 
-	/**
-	 * @var RouteParser
-	 */
-	private $parser;
+    /**
+     * @var RouteParser
+     */
+    private $parser;
 
-	/**
-	 * @var string
-	 */
-	private $group = '';
+    /**
+     * @var string
+     */
+    private $group = '';
 
-	public function __construct(RouteParser $parser = null)
-	{
-		$this->parser = $parser ?? new RouteParser;
-	}
+    public function __construct(RouteParser $parser = null)
+    {
+        $this->parser = $parser ?? new RouteParser;
+    }
 
-	/**
-	 * Add URI route with method and handler.
-	 *
-	 * @param array $method Route method.
-	 * @param string $route Route path.
-	 * @param mixed $handler Route handler.
-	 * @return void
-	 */
-	public function addRoute(
-		array $methods,
-		string $route,
-		$handler,
-		array $pattern = []
-	) {
-		$this->parser->reset();
-		$this->parser->parse(
-			sprintf("%s/%s", $this->getGroup(), $route),
-			$pattern
-		);
+    /**
+     * Add URI route with method and handler.
+     *
+     * @param array $method Route method.
+     * @param string $route Route path.
+     * @param mixed $handler Route handler.
+     * @return void
+     */
+    public function addRoute(
+        array $methods,
+        string $route,
+        $handler,
+        array $pattern = []
+    ) {
+        $this->parser->reset();
+        $this->parser->parse(
+            sprintf("%s/%s", $this->getGroup(), $route),
+            $pattern
+        );
 
-		$this->routes[] = [
-			'method' => $methods,
-			'route' => $this->parser->getSegments(),
-			'placeholder' => $this->parser->getParameters(),
-			'handler' => $handler
-		];
-	}
+        $this->routes[] = [
+            'method' => $methods,
+            'route' => $this->parser->getSegments(),
+            'placeholder' => $this->parser->getParameters(),
+            'handler' => $handler
+        ];
+    }
 
-	/**
-	 * Return aggregated HTTP route.
-	 *
-	 * @return array
-	 */
-	public function getRoutes()
-	{
-		return $this->routes;
-	}
+    /**
+     * Return aggregated HTTP route.
+     *
+     * @return array
+     */
+    public function getRoutes()
+    {
+        return $this->routes;
+    }
 
-	/**
-	 * Set routes metadata.
-	 *
-	 * @param array $routes Routes metadata.
-	 * @return void
-	 */
-	public function setRoutes(array $routes)
-	{
-		$this->routes = $routes;
-	}
+    /**
+     * Set routes metadata.
+     *
+     * @param array $routes Routes metadata.
+     * @return void
+     */
+    public function setRoutes(array $routes)
+    {
+        $this->routes = $routes;
+    }
 
-	/**
-	 * Set route group.
-	 *
-	 * @param string $group Route group.
-	 * @return void
-	 */
-	public function setGroup(string $group)
-	{
-		$this->group = rtrim($group, '/');
-	}
+    /**
+     * Set route group.
+     *
+     * @param string $group Route group.
+     * @return void
+     */
+    public function setGroup(string $group)
+    {
+        $this->group = rtrim($group, '/');
+    }
 
-	/**
-	 * Get route group.
-	 *
-	 * @return string
-	 */
-	public function getGroup()
-	{
-		return $this->group;
-	}
+    /**
+     * Get route group.
+     *
+     * @return string
+     */
+    public function getGroup()
+    {
+        return $this->group;
+    }
 
-	/**
-	 * Reset route group.
-	 *
-	 * @return void
-	 */
-	public function resetGroup()
-	{
-		$this->group = '';
-	}
+    /**
+     * Reset route group.
+     *
+     * @return void
+     */
+    public function resetGroup()
+    {
+        $this->group = '';
+    }
 }
