ad217fbaf7da41da3904dd8f3e3948406b252e02
plvhx <gandung@slackerc0de.us>
2020-07-28 16:17:47 +0700

    Refactoring src/Response/ResponseAccessor.php

--- src/Response/ResponseAccessor.php
+++ src/Response/ResponseAccessor.php
@@ -9,44 +9,44 @@ use LilleBitte\Routing\Exception\DispatchedResponseException;
  */
 class ResponseAccessor implements ResponseAccessorInterface
 {
-	/**
-	 * @var array
-	 */
-	private $response;
+    /**
+     * @var array
+     */
+    private $response;
 
-	public function __construct(array $response)
-	{
-		$this->setDispatchedResponse($response);
-	}
+    public function __construct(array $response)
+    {
+        $this->setDispatchedResponse($response);
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	public function getStatus()
-	{
-		return isset($this->response['status'])
-			? $this->response['status']
-			: null;
-	}
+    /**
+     * {@inheritdoc}
+     */
+    public function getStatus()
+    {
+        return isset($this->response['status'])
+            ? $this->response['status']
+            : null;
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	public function getResponse()
-	{
-		return isset($this->response['response'])
-			? $this->response['response']
-			: null;
-	}
+    /**
+     * {@inheritdoc}
+     */
+    public function getResponse()
+    {
+        return isset($this->response['response'])
+            ? $this->response['response']
+            : null;
+    }
 
-	/**
-	 * Set dispatched response.
-	 *
-	 * @param array $response Dispatched response.
-	 * @return void
-	 */
-	private function setDispatchedResponse(array $response)
-	{
-		$this->response = $response;
-	}
+    /**
+     * Set dispatched response.
+     *
+     * @param array $response Dispatched response.
+     * @return void
+     */
+    private function setDispatchedResponse(array $response)
+    {
+        $this->response = $response;
+    }
 }
